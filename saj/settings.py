208e992f320138e7af66ac62f78101a8cd351434
Mohammad Yusuf Been Hashem <yusuf@graduate.utm.my>
2022-01-15 21:06:00 +0800

    cdn

--- saj/settings.py
+++ saj/settings.py
@@ -35,7 +35,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'd!m50t)w$$&ff(*pn7%oqw-1yxo+eub*xcxd^8pzo=*2)ynq=w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1','localhost', 'saj-dashboard.herokuapp.com']
 
